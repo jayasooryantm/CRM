7a5c280432b03014acfe9af012cf8dd47b112796
jayasooryantm <jayasooryan.tm@outlook.com>
2023-10-31 21:16:54 +0530

    user registration

--- dcrm/webapp/urls.py
+++ dcrm/webapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register")
 ]
